682193edc76e0001d9e2736d12304751dbe4a5c2
BabaDorin <bvd.dorin@gmail.com>
2020-03-18 12:53:21 +0200

    Button Events Implemented

--- MatematicaBacMD/MatematicaBacMD/MainPage.xaml.cs
+++ MatematicaBacMD/MatematicaBacMD/MainPage.xaml.cs
@@ -58,7 +58,139 @@ namespace MatematicaBacMD
             await Navigation.PushAsync(new Functii());
         }
 
+        async void btnInecuatii(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Inecuatii());
+        }
 
+        async void btnTrigonometrie(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Trigonometrie());
+        }
+        async void btnSiruriNumerice(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SiruriNumerice());
+        }
+
+        async void btnNumereComplexe(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new NumereComplexe());
+        }
+
+        async void btnMatrici(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Matrici());
+        }
+
+        async void btnLimite(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Limite());
+        }
+
+        async void btnDerivateleFunctiilor(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new DerivateleFunctiilor());
+        }
+
+        async void btnIntegrale(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Integrale());
+        }
+
+        async void btnPrimitiveleFunctiilor(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PrimitiveleFunctiilor());
+        }
+
+        async void btnTeoriaProbabilitatilor(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new TeoriaProbabilitatilor());
+        }
+
+        async void btnStatistica(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Statistica());
+        }
+
+        async void btnTriunghiul(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Triunghiul());
+        }
+
+        async void btnParalelogramul(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Paralelogramul());
+        }
+
+        async void btnTrapezul(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Trapezul());
+        }
+
+        async void btnThales(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Thales());
+        }
+
+        async void btnPoligoane(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Poligoane());
+        }
+
+        async void btnAriiFiguriPlane(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AriiFiguriPlane());
+        }
+
+        async void btnParalelism(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Paralelism());
+        }
+
+        async void btnPerpendicularitate(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Perpendicularitate());
+        }
+
+        async void btnPrisma(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Prisma());
+        }
+
+        async void btnPiramida(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Piramida());
+        }
+
+        async void btnTrunchiPiramida(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new TrunchiPiramida());
+        }
+
+        async void btnCilindru(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Cilindru());
+        }
+
+        async void btnCon(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Conul());
+        }
+
+        async void btnTrunchiCon(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new TrunchiCon());
+        }
+
+        async void btnSfera(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Sfera());
+        }
+
+        async void btnVolumPoliedre(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new VolumPoliedre());
+        }
 
     }
 }

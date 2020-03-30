5dd295a798c52141a41d575ce86aff150fb03fd4
BabaDorin <bvd.dorin@gmail.com>
2020-03-30 19:27:38 +0300

    Conul Done

--- MatematicaBacMD/MatematicaBacMD/Views/Exercitii/exCombinatorica.xaml.cs
+++ MatematicaBacMD/MatematicaBacMD/Views/Exercitii/exCombinatorica.xaml.cs
@@ -16,5 +16,10 @@ namespace MatematicaBacMD.Views.Exercitii
 		{
 			InitializeComponent ();
 		}
-	}
+
+        async void btnLoad(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new exTeoriaProbabilitatilor());
+        }
+    }
 }

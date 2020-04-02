cf6d3d6cc829af35c20fd90337d2be28b7642f50
BabaDorin <bvd.dorin@gmail.com>
2020-04-03 02:33:42 +0300

    SplashScreen (Maybe) Done

--- MatematicaBacMD/MatematicaBacMD.Android/MainActivity.cs
+++ MatematicaBacMD/MatematicaBacMD.Android/MainActivity.cs
@@ -11,7 +11,7 @@ namespace MatematicaBacMD.Droid
 {
 
     // Icon = "@mipmap/icon"
-    [Activity(Label = "Matematica", Icon = "@drawable/ilogo", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Matematica", Icon = "@drawable/ilogo", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
---
+++ MatematicaBacMD/MatematicaBacMD.Android/SplashActivity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System.Threading.Tasks;
+
+namespace MatematicaBacMD.Droid
+{
+    [Activity(Theme ="@style/Theme.Splash",
+        MainLauncher =true,
+        NoHistory =true)]
+    public class SplashActivity:Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupwork = new Task(() => { SimulateStartup(); });
+            startupwork.Start();
+        }
+
+        async void SimulateStartup()
+        {
+            await Task.Delay(2000);
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}

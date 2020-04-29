574b41c8203b1a547a2941838370534da925fc78
BabaDorin <bvd.dorin@gmail.com>
2020-04-29 03:02:21 +0300

    FFImageLoading and Adding Resources to iOS version

--- MatematicaBacMD/MatematicaBacMD.iOS/AppDelegate.cs
+++ MatematicaBacMD/MatematicaBacMD.iOS/AppDelegate.cs
@@ -4,6 +4,7 @@ using System.Linq;
 
 using Foundation;
 using UIKit;
+using FFImageLoading.Forms;
 
 namespace MatematicaBacMD.iOS
 {
@@ -23,6 +24,7 @@ namespace MatematicaBacMD.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
+            FFImageLoading.Forms.Platform.CachedImageRenderer.Init();
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
